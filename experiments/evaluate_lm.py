ef564091582bbecb0db048356c24ca0be690434e
Miruna Pislar <miruna.pislar@gmail.com>
2019-04-15 12:15:09 +0300

    Polished a bit the code for the language model evaluation.

--- experiments/evaluate_lm.py
+++ experiments/evaluate_lm.py
@@ -13,20 +13,72 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("--data", type=str, default="data/generated.tab",
                     help="Path to the data file to be evaluated.")
+parser.add_argument("--original", type=bool, default=False,
+                    help="Whether the data file is in the original format"
+                         "(as in Gulordava et al.'s \"generated.tab\") or not.")
 parser.add_argument("--vocab", type=str, default="data/",
                     help="Path to the directory to the corpus "
                          "vocabulary (of the language model).")
 parser.add_argument("--checkpoint", type=str,
                     help="Language model checkpoint to be used.")
+parser.add_argument("--mask_target", type=bool, default=False,
+                    help="Whether to mask or not the target word(s).")
 parser.add_argument("--seed", type=int, default=532,
                     help="Random seed")
 parser.add_argument("--cuda", action="store_true",
                     help="use CUDA")
 args = parser.parse_args()
-eval_batch_size = 1
+
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: CUDA device available. Consider running with --cuda.")
+    else:
+        torch.cuda.manual_seed(args.seed)
 
 
-def read_datafile(mask_target=False):
+def read_datafile():
+    """
+    Reads one row at a time from a data file.
+    The mandatory fields of the header are:
+        * sent - the sentence to be evaluated by the language model
+               - it needs to be the same for pairs of grammatical
+                and ungrammatical sentences
+        * form - the correct form of the word to be evaluated (target)
+        * form_alt - the incorrect form of the word to be evaluated (target)
+        * len_prefix - length of the sentence up to the form/form_alt word
+                       i.e. index (position) of the target
+    """
+    rows = csv.DictReader(open(args.data, encoding="utf8"), delimiter="\t")
+    data = []
+    for row in rows:
+        sent = row["sent"].split()
+        good_form = row["form"]
+        bad_form = row["form_alt"]
+        target_idx = row["len_prefix"]
+        if args.mask_target:
+            sent[int(target_idx)] = "***mask***"
+        sent = " ".join(sent)
+        data.append((sent, target_idx, good_form, bad_form))
+    return data
+
+
+def read_datafile_original():
+    """
+    Reads two rows at a time from a data file: the first row is always
+    the grammatical one, the second one is the ungrammatical one.
+    This implementation allows for an easier evaluation of the data
+    files in the same format as originally proposed by Gulordava et al.
+    The mandatory fields of the header are:
+        * sent - the sentence to be evaluated by the language model
+               - it needs to be the same for pairs of grammatical
+                and ungrammatical sentences
+        * class - can be either "correct" or "wrong" (always in this order)
+        * form - the form of the word to be evaluated (e.g. "is" for the
+                grammatical example and "are" for the ungrammatical one)
+        * len_prefix - length of the sentence up to the form word;
+                       this is the index of the target (e.g. "is" or "are")
+    """
     rows = csv.DictReader(open(args.data, encoding="utf8"), delimiter="\t")
     data = []
     for row in rows:
@@ -38,7 +90,7 @@ def read_datafile(mask_target=False):
         good_form = row["form"]
         bad_form = row2["form"]
         target_idx = row["len_prefix"]
-        if mask_target:
+        if args.mask_target:
             sent[int(target_idx)] = "***mask***"
         sent = " ".join(sent)
         data.append((sent, target_idx, good_form, bad_form))
@@ -46,14 +98,9 @@ def read_datafile(mask_target=False):
 
 
 def load_model():
-    torch.manual_seed(args.seed)
-    if torch.cuda.is_available():
-        if not args.cuda:
-            print("WARNING: you have a CUDA device available. "
-                  "Consider running with --cuda.")
-        else:
-            torch.cuda.manual_seed(args.seed)
-
+    """
+    Load a previously trained (on CUDA) language model.
+    """
     with open(args.checkpoint, "rb") as f:
         print("Loading the model...")
         if args.cuda:
@@ -70,77 +117,67 @@ def load_model():
 
 
 def repackage_hidden(hidden):
-    # Detach hidden states from their history.
+    """
+    Detach hidden states from their history.
+    """
     if isinstance(hidden, torch.Tensor):
         return hidden.detach()
     else:
         return tuple(repackage_hidden(v) for v in hidden)
 
 
-def batchify(data, cuda):
-    # Work out how cleanly we can divide the dataset into bsz parts.
-    nbatch = data.size(0) // eval_batch_size
-    # Trim off any extra elements that wouldn't cleanly fit (remainders).
-    data = data.narrow(0, 0, nbatch * eval_batch_size)
-    # Evenly divide the data across the bsz batches.
-    data = data.view(eval_batch_size, -1).t().contiguous()
-    if cuda:
-        data = data.cuda()
-    return data
-
-
-def get_scores_for_words(
-        model, hidden, dictionary, sent, target_idx,
-        good, bad, mask_target=False):
-    if mask_target:
-        pre, target, post = sent.split("***")
-        if "mask" in target.lower():
-            target = "[MASK]"
-        all_token_idx = tokenize_sentence(dictionary, pre + target + post)
-    else:
-        all_token_idx = tokenize_sentence(dictionary, sent)
-    try:
-        word_ids = [dictionary.word2idx[good], dictionary.word2idx[bad]]
-    except KeyError:
-        print("Skipping bad wins: %s and %s." % (good, bad))
-        return None
-    tokens_batch = batchify(all_token_idx, args.cuda)
-    output, hidden = model(tokens_batch, hidden)
-    output_flat = output.view(-1, len(dictionary))
-    log_probs = F.log_softmax(output_flat).data
-    log_probs_np = log_probs.cpu().numpy()
-
-    # Create mask for the target word prediction
-    mask = [False] * len(all_token_idx)
-    mask[int(target_idx) - 1] = True
-    mask = array(mask)
-
-    # Get scores for the masked target word
-    res = log_probs_np[mask][0]
-    scores = res[word_ids]
-    return [float(x) for x in scores], hidden
-
-
 def evaluate(data, model, dictionary):
+    """
+    Evaluate a language model on certain data examples.
+    :param data: data to be evaluated by the language model
+    :param model: a pre-trained language model
+    :param dictionary: the vocabulary of the corpus on which
+                       the language model has been trained
+    """
     model.eval()
-    hidden = model.init_hidden(eval_batch_size)
-    for (masked_sent, target_idx, good, bad) in tqdm(data):
-        scores, hidden = get_scores_for_words(
-            model, hidden, dictionary, masked_sent,
-            target_idx, good, bad)
-        hidden = repackage_hidden(hidden)
-        if scores is None:
+    hidden = model.init_hidden(bsz=1)
+    for (sent, target_idx, good, bad) in tqdm(data):
+        if args.mask_target:
+            pre, target, post = sent.split("***")
+            if "mask" in target.lower():
+                target = "[MASK]"
+            all_token_idx = tokenize_sentence(dictionary, pre + target + post)
+        else:
+            all_token_idx = tokenize_sentence(dictionary, sent)
+        try:
+            word_ids = [dictionary.word2idx[good], dictionary.word2idx[bad]]
+        except KeyError:
+            print("Skipping bad wins: %s and %s." % (good, bad))
             continue
+        tokens_batch = all_token_idx.view(1, -1).t().contiguous()
+        output, hidden = model(tokens_batch, hidden)
+        output_flat = output.view(-1, len(dictionary))
+        log_probs = F.log_softmax(output_flat).data
+        log_probs_np = log_probs.cpu().numpy()
+
+        # Create mask for the target word prediction
+        mask = [False] * len(all_token_idx)
+        mask[int(target_idx) - 1] = True
+        mask = array(mask)
+
+        # Get scores for the masked target word
+        res = log_probs_np[mask][0]
+        scores = [float(x) for x in res[word_ids]]
+
+        hidden = repackage_hidden(hidden)
         good_score = scores[0]
         bad_score = scores[1]
         print(str(good_score > bad_score),
               good, good_score, bad, bad_score,
-              masked_sent.encode("utf8"), sep=u"\t")
+              sent.encode("utf8"), sep=u"\t")
 
 
 def main():
     # Read and load the data to be evaluated
-    data = read_datafile()
+    if args.original:
+        data = read_datafile_original()
+    else:
+        data = read_datafile()
 
     # Load the corpus dictionary
     dictionary = Dictionary(args.vocab)
